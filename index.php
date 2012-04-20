97c1a17d9a137f3645ea43369bfc1c9b277df541
Daniel Pepin <donutdan4114@gmail.com>
2012-04-20 00:37:51 -0400

    updated, trying .php

---
+++ index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>4Bytes Framework (PHP)</title>
+
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
+    <link rel="stylesheet" href="stylesheets/custom.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <img src="images/logo_medium.png" width="260" height="260" alt="Main Logo"/>
+        <h1>4Bytes Framework</h1>
+        <hr />
+        <p>4Bytes is a super easy, light-weight PHP framework. It is modular, extensible, and fast.</p>
+        <p class="view"><a href="https://github.com/donutdan4114/4Bytes-framework">View the Project on GitHub</a></p>
+        <ul>
+          <li><a href="https://github.com/donutdan4114/4Bytes-framework/zipball/master">Download <strong>ZIP File</strong></a></li>
+          <li><a href="https://github.com/donutdan4114/4Bytes-framework/tarball/master">Download <strong>TAR Ball</strong></a></li>
+          <li><a href="https://github.com/donutdan4114/4Bytes-framework">Fork On <strong>GitHub</strong></a></li>
+        </ul>
+      </header>
+      <section>
+        <h3>About 4Bytes</h3>
+
+        <p>
+          4Bytes is designed to change the way you make PHP applications. 
+          This easy-to-use framework is designed around being the fastest, easiest, and <i>sexiest</i> framework.
+          Yes, I said <i>sexy</i> when describing a PHP framework.
+        </p>
+        <p>
+          The main goal of 4Bytes is ease-of-use. Nothing is more frustrating, especially to a new PHP developer, 
+          than trying to learn a complex framework <b>on top of</b> learning PHP. 
+          With the 4Bytes Framework, you get a really powerful system, whilst being easy to read/code.
+        </p>
+        <p>
+          Check out the code snippets below to get a taste of 4Bytes:
+        </p>
+
+        <p class="description">
+          4Bytes is a module driven framework. You can make modules, or download pre-made ones.
+        </p>
+
+        <ol class="code">
+          <li><code>test</code></li>
+          <li><code>  test</code></li>
+          <li><code>  test</code></li>
+          <li><code>test</code></li>
+        </ol>
+
+        <?php echo 'test'; ?>
+
+        <p class="description"></p>
+        <ol class="code">
+          <li><code>test</code></li>
+          <li><code>  test</code></li>
+          <li><code>  test</code></li>
+          <li><code>test</code></li>
+        </ol>
+
+        <p class="description">
+          4Bytes is a module driven framework. You can make modules, or download pre-made ones.
+        </p>
+        <ol class="code">
+          <li><code>test</code></li>
+          <li><code>  test</code></li>
+          <li><code>  test</code></li>
+          <li><code>test</code></li>
+        </ol>
+
+
+        <p>4Bytes makes it easy to create menu items, cache template files, and turns AJAX into your b**ch</p>
+
+        <h3>Coming soon...</h3>
+
+        <p>There is some <i>serious</i> work left to be done. Hopefully this project will be at v1.0 by the end of 2012.</p>
+
+
+      </section>
+      <footer>
+        <p>This project is maintained by <a href="https://github.com/donutdan4114">donutdan4114</a></p>
+      </footer>
+    </div>
+    <script src="javascripts/scale.fix.js"></script>
+
+  </body>
+</html>
