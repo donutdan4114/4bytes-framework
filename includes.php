9e5b90b9bfc648d228163dd197c9075003fd10b6
Daniel Pepin <dpepin@digitalbungalow.com>
2013-01-06 21:36:35 -0500

    Added a cookie and session file for easyness.

--- includes.php
+++ includes.php
@@ -2,4 +2,5 @@
 
 // require 'includes/json.inc'; // Loads the JSON file database system.
 // require 'includes/mysql.inc'; // Connects to MySQL database.
-
+// require 'includes/cookie.inc'; // Cookie helper.
+// require 'includes/session.inc'; // Session helper.
