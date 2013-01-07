c4300c17488322e130ac94ff5559a1077ab6d6cb
Daniel Pepin <dpepin@digitalbungalow.com>
2013-01-06 21:46:00 -0500

    Updated routes documentation.

--- routes.php
+++ routes.php
@@ -3,14 +3,6 @@
  * Routes allow you to manipulate URLs.
  * All URLs are naturally /controller/method/arg1/arg2/etc...
  * You can style URLs so they don't have to exactly match up with controllers.
- * Example:
- *   Instead of using /blog/view/45
- *   You could set a route for:
- *   addRoute(array('blog/best-php-frameworks'), array(
- *     'controller' => 'blog',
- *     'method' => 'view',
- *     'args' => array(45),
- *   ));
  */
 
 /**
